d640f323ee4e6c956b4e9abbadb8a0659b12efe3
Selmi Med Dhia <monjihidousi@gmail.com>
2025-02-20 21:16:19 +0100

    PID working correctly

--- line_follower.ino
+++ line_follower.ino
@@ -2,11 +2,11 @@ int flag;
 bool PIDyes;
 int threashhold[8];
 int sensors[8] = {4,12,13,14,25,26,27,35};
-int weights[8] = {10,8,7,5,5,7,8,10};
+int weights[8] = {-15,-9,-6,-5,5,6,9,15};
 int oldSums[8] = {0,0,0,0,0};
-int oldTimes[8] = {0,0,0,0,0};
-int lastMeasure;
-int kp, kd, ki, ks;
+unsigned long oldTimes[8] = {0,0,0,0,0};
+int kp, kd, ki;
+float ks;
 int led;
 int baseSpeedRight;
 int baseSpeedLeft;
@@ -21,16 +21,13 @@ int trig, echo;
 int getPIDValue(){
   int sum = 0;
   for(int j=0; j<4; j++){
-    for(int i=0; i<4; i++){
+    for(int i=0; i<8; i++){
       sum += getValue(i) * weights[i];
     }
-    for(int i=4; i<8; i++){
-      sum -= getValue(i) * weights[i];
-    }
-    delayMicroseconds(100);
+    delayMicroseconds(200);
   }
-  int currentMeasure = millis();
-  int value =    kp*sum   +    kd*(sum - oldSums[4])/(currentMeasure-oldTimes[4])   +  ki*(sum + oldSums[4])*(currentMeasure-oldTimes[4])/2 ;
+  unsigned long currentMeasure = millis();
+  int value =    kp*sum   +    kd*(sum - oldSums[4])/(3)   +  ki*(sum + oldSums[4])*(3)/2 ;
   for(int i=1;i<4; i++){
     oldSums[i] = oldSums[i-1];
     oldTimes[i] = oldTimes[i-1];
@@ -88,11 +85,10 @@ void speedLeft(int speed){
 void setup() {
   flag = 0;
   PIDyes = false;
-  lastMeasure = millis();
   kp = 1;
-  kd = 2;
+  kd = 1;
   ki = 0;
-  ks = 1;
+  ks = 1.0;
   led = 32;
   pushButton = 33;
   trig = 5;
@@ -104,13 +100,16 @@ void setup() {
   rightMotorA = 21;
   leftMotorA = 23;
   pinMode(led, OUTPUT);
-  pinMode(pushButton, INPUT);
+  pinMode(pushButton, INPUT_PULLUP);
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
+  pinMode(rightMotorB,OUTPUT);
+  pinMode(leftMotorB,OUTPUT);
+  pinMode(rightMotorA,OUTPUT);
+  pinMode(leftMotorA,OUTPUT);
   delay(1000);
   calibrate();
-  
-  while (digitalRead(pushButton) == 0){
+  while (digitalRead(pushButton) == 1){
     delay(1);
   };
   delay(1000);
@@ -119,13 +118,10 @@ void setup() {
   
   speedRight(baseSpeedRight);
   speedLeft(baseSpeedLeft);
-  
   Serial.begin(9600);
 }
 void loop() {
+  speedRight(min(255,  (int)( (baseSpeedRight + getPIDValue() * ks ) )) );
+  speedLeft(min(255,(int)( (baseSpeedLeft - getPIDValue() * ks ) ) ) );
   
-  speedRight((baseSpeedRight - getPIDValue() * ks)%255 );
-  speedLeft((baseSpeedLeft + getPIDValue() * ks)%255 );
-  
-  Serial.println(getPIDValue());
 }
